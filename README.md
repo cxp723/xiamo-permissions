8e8e6d7ff39c15e3fefce472727670e5c6454059
Alexander Kirichenko <alexander@aporia.su>
2023-08-09 14:57:59 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# react-native-xiaomi-permissions
+
+Native module for Xiaomi specific permissions
+
+## Installation
+
+```sh
+npm install react-native-xiaomi-permissions
+```
+
+## Usage
+
+```js
+import { multiply } from 'react-native-xiaomi-permissions';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ android/src/main/java/com/xiaomipermissions/XiaomiPermissionsModule.java
@@ -0,0 +1,32 @@
+package com.xiaomipermissions;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = XiaomiPermissionsModule.NAME)
+public class XiaomiPermissionsModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "XiaomiPermissions";
+
+  public XiaomiPermissionsModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
